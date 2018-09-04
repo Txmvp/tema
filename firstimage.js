dc6c221e4e17c03eb16f0c6ab8c84c7674da8d19
Riyan <37412329+Txmvp@users.noreply.github.com>
2018-09-04 19:10:18 +0700

    Add files via upload

---
+++ firstimage.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(function() {
+   $(".separator:first").remove();
+      $(".post-body > img:first").remove();
+                      });
+//]]>
+</script>
